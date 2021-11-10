beac24a3bc3e19508ede22df664ed2e71a108f4a
jatrindo <jatrindo4096@gmail.com>
2021-11-09 23:39:58 -0600

    Add program TODOs for CoffeeMachine project

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -29,3 +29,12 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+# Coffee Machine Program Requirements
+# TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+# TODO: 2. Turn off the Coffee Machine by entering "off" to the prompt.
+# TODO: 3. Print report of all coffee machine resources
+# TODO: 4. Check resources are sufficient to make drink order
+# TODO: 5. Process coins.
+# TODO: 6. Check that the transaction is successful (i.e. enough resources + money)
+# TODO: 7. Make the coffee if the transaction is successful
