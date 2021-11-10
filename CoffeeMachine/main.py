5c9a5e90ef52a9279d755df466325755b841758f
jatrindo <jatrindo4096@gmail.com>
2021-11-10 00:46:12 -0600

    Complete TODOs 1-4 of CoffeeMachine project
    TD 1: Add while loop that prompts the user to enter a drink
    TD 2; Add 'off' command to while loop to end the program
    TD 3: Add 'report' command to print current coffee machine resources
    TD 4: Add "enough_*" family of functions to check whether enough
    resources are available to make the specified drink

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,11 +32,74 @@ resources = {
     "coffee": 100,
 }
 
+money = 0.0
+
 # Coffee Machine Program Requirements
-# TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
-# TODO: 2. Turn off the Coffee Machine by entering "off" to the prompt.
-# TODO: 3. Print report of all coffee machine resources
-# TODO: 4. Check resources are sufficient to make drink order
+# 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+# 2. Turn off the Coffee Machine by entering "off" to the prompt.
+# 3. Print report of all coffee machine resources
+# 4. Check resources are sufficient to make drink order
 # TODO: 5. Process coins.
 # TODO: 6. Check that the transaction is successful (i.e. enough resources + money)
 # TODO: 7. Make the coffee if the transaction is successful
+
+
+def print_report():
+    print(dedent(f"""\
+        Water: {resources['water']}ml
+        Milk: {resources['milk']}ml
+        Coffee: {resources['coffee']}g
+        Money: ${money}\
+    """))
+
+
+def enough_money(drink, amount_given):
+    return amount_given >= MENU[drink]['cost']
+
+
+def enough_water(drink):
+    return resources[drink] >= MENU[drink]['ingredients']['water']
+
+
+def enough_milk(drink):
+    return resources[drink] >= MENU[drink]['ingredients']['milk']
+
+
+def enough_coffee(drink):
+    return resources[drink] >= MENU[drink]['ingredients']['coffee']
+
+
+def enough_resources(drink):
+    """
+    Checks if we have enough resources to make the specified drink.
+
+    :param drink: The coffee we want to check whether we have enough ingredients for
+    :return: True if there are enough ingredients, otherwise print an error message and return False
+    """
+    if not enough_water(drink):
+        print("Sorry, there is not enough water.")
+        return False
+
+    if not enough_milk(drink):
+        print("Sorry, there is not enough milk.")
+        return False
+
+    if not enough_coffee(drink):
+        print("Sorry, there is not enough coffee.")
+        return False
+
+    return True
+
+
+while True:
+    response = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if response == 'espresso' or response == 'latte' or response == 'cappuccino':
+        pass
+    elif response == 'report':
+        print_report()
+    elif response == 'off':
+        break
+    else:
+        print("Please make a valid selection")
+
