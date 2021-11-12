adb10c083aef6b5b1184818bf0261ccbd0f63ac5
jatrindo <jatrindo4096@gmail.com>
2021-11-11 18:46:51 -0600

    Create QuizBrain class and display first question

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -1,8 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for qdata in question_data:
     text = qdata.get('text')
     answer = qdata.get('answer')
     question_bank.append(Question(text, answer))
+
+qbrain = QuizBrain(question_bank)
+qbrain.next_question()
