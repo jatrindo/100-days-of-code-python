3f5e130bb940d83da2d6abb330b01015f3db992f
jatrindo <jatrindo4096@gmail.com>
2021-11-19 10:24:19 -0600

    Complete all TODOs of MailMerge project
    Code now makes a unique letter for each name in invited_names.txt and
    the letter template.
    
    Output letters are included with the commit.

--- MailMerge/main.py
+++ MailMerge/main.py
@@ -1,8 +1,19 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# File paths
+invited_names_path = "Input/Names/invited_names.txt"
+starting_letter_path = "Input/Letters/starting_letter.txt"
+letter_filename_path_template = "Output/ReadyToSend/letter_for_{name}.txt"
+
+# Read in names
+with open(invited_names_path) as f:
+    names = f.readlines()
+
+# Read in template
+with open(starting_letter_path) as f:
+    letter_content = f.read()
+
+# Make a unique letter for each individual name
+for name in names:
+    name = name.strip()
+    letter_filename_path = letter_filename_path_template.replace('{name}', name)
+    with open(letter_filename_path, 'w') as f:
+        f.write(letter_content.replace('[name]', name))
