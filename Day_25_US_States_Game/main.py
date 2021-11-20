5448e7a79f893d1d651bc9427ee6bc676ad8da5e
jatrindo <jatrindo4096@gmail.com>
2021-11-19 20:20:41 -0600

    Add TODOs for Day 25 US States Game project

--- Day_25_US_States_Game/main.py
+++ Day_25_US_States_Game/main.py
@@ -9,3 +9,10 @@ turtle.shape(image)
 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 print(answer_state)
+
+# TODO: 1. Convert the guess to Title case
+# TODO: 2. Check if the guess is among the 50 states
+# TODO: 3. Write correct guesses onto the map
+# TODO: 4. Use a loop to allow the user to keep guessing
+# TODO: 5. Record the correct guesses in a list
+# TODO: 6. Keep track of the score
