9e9be2fd6347d1f44de9eb7c2fe178dbaebaa3a7
jatrindo <jatrindo4096@gmail.com>
2021-11-20 19:24:37 -0600

    Update Day 25 US States game project
    - Add code to exit the game when the user clicks 'Cancel'
    - Write out states missed by the end of the game to a csv for further
    study

--- Day_25_US_States_Game/main.py
+++ Day_25_US_States_Game/main.py
@@ -15,21 +15,27 @@ t.hideturtle()
 
 # Read in the states data
 state_df = pandas.read_csv('50_states.csv')
+states = state_df.state.to_list()
 
 # 5. Record the correct guesses in a list
 guessed_states = []
 
 # 4. Use a loop to allow the user to keep guessing (user can end the game by
-#   clicking 'Cancel'
+#   clicking 'Cancel')
 title = "Guess the state"
 while len(guessed_states) < len(state_df):
     # 1. Convert the guess to Title case
-    response = screen.textinput(title=title, prompt="What's another state's name?").title()
+    response = screen.textinput(title=title, prompt="What's another state's name?")
+
+    if response is None or response == "Exit":
+        break
+
+    response = response.title()
 
     # 2. Check if the guess is among the 50 states
-    state_row = state_df[state_df['state'] == response]
+    if response not in guessed_states and response in states:
+        state_row = state_df[state_df['state'] == response]
 
-    if response not in guessed_states and len(state_row) > 0:
         # 3. Write correct guesses onto the map
         x = int(state_row.x)
         y = int(state_row.y)
@@ -41,4 +47,15 @@ while len(guessed_states) < len(state_df):
     # 6. Keep track of the score (which are the number of states guessed)
     title = f"{len(guessed_states)}/{len(state_df)} States Correct"
 
-screen.exitonclick()
+# Write out states missed for further review
+if len(guessed_states) < len(states):
+    states_to_learn = []
+    for state in states:
+        if state not in guessed_states:
+            states_to_learn.append(state)
+
+    df = pandas.DataFrame({
+        'states': states_to_learn
+    })
+
+    df.to_csv('states_to_learn.csv')
