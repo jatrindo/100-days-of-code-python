b5f3947a9563384cd743eae8af7af2ae34205054
jatrindo <jatrindo4096@gmail.com>
2021-11-24 14:19:13 -0600

    Add tomato image and timer text to Pomodoro App

--- Day_28_Pomodoro_App/main.py
+++ Day_28_Pomodoro_App/main.py
@@ -1,4 +1,4 @@
-
+import tkinter as tk
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.pack()
+
+window.mainloop()
