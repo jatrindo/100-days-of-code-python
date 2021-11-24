4be83a4603f171b8d36d0b3696faad3d65e8b74f
jatrindo <jatrindo4096@gmail.com>
2021-11-24 14:36:15 -0600

    Update Pomodoro App layout
    - Add start, reset buttons
    - Add title
    - Add checkmark

--- Day_28_Pomodoro_App/main.py
+++ Day_28_Pomodoro_App/main.py
@@ -9,9 +9,13 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    pass
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    pass
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -20,10 +24,24 @@ window = tk.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Labels
+title_label = tk.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
+title_label.grid(row=0, column=1)
+
+checkmark_label = tk.Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "bold"))
+checkmark_label.grid(row=3, column=1)
+
+# Tomato Image with Timer
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+# Buttons
+start_button = tk.Button(text="Start", command=start_timer, highlightthickness=0)
+start_button.grid(row=2, column=0)
+reset_button = tk.Button(text="Reset", command=reset_timer, highlightthickness=0)
+reset_button.grid(row=2, column=2)
 
 window.mainloop()
