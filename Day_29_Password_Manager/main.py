e85860a88dd489c03df24699647b46b0a8da020e
jatrindo <jatrindo4096@gmail.com>
2021-12-01 18:04:46 -0600

    Add lock image to Password Manager App

--- Day_29_Password_Manager/main.py
+++ Day_29_Password_Manager/main.py
@@ -1,5 +1,19 @@
+import tkinter as tk
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+WHITE = "#FFFFFF"
+
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg=WHITE)
+
+canvas = tk.Canvas(width=200, height=200, bg=WHITE, highlightthickness=0)
+lockImg = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lockImg)
+canvas.grid(row=0, column=0)
+
+window.mainloop()
