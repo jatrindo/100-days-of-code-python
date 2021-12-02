817d366ca239bcdad5771ae9dc027aed1c71e225
jatrindo <jatrindo4096@gmail.com>
2021-12-01 23:38:33 -0600

    Add password save feature and reorganize code

--- Day_29_Password_Manager/main.py
+++ Day_29_Password_Manager/main.py
@@ -3,6 +3,20 @@ import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+password_file = "data.txt"
+
+
+def save():
+    website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+
+    with open(password_file, 'a') as f:
+        f.write(f"{website} | {email} | {password}\n")
+
+    # Reset the website, password fields
+    website_entry.delete(0, tk.END)
+    password_entry.delete(0, tk.END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 WHITE = "#FFFFFF"
@@ -18,32 +32,36 @@ lockImg = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lockImg)
 canvas.grid(row=0, column=1)
 
-# Website Row
-website_label = tk.Label(text="Website:")
+
+# Labels
+website_label = tk.Label(text="Website:", bg=WHITE)
 website_label.grid(row=1, column=0)
 
+email_username_label = tk.Label(text="Email/Username:", bg=WHITE)
+email_username_label.grid(row=2, column=0)
+
+password_label = tk.Label(text="Password:", bg=WHITE)
+password_label.grid(row=3, column=0)
+
+
+# Entries
 website_entry = tk.Entry(width=45)
 website_entry.grid(row=1, column=1, columnspan=2)
 
-# Email/Username Row
-email_username_label = tk.Label(text="Email/Username:")
-email_username_label.grid(row=2, column=0)
-
 email_username_entry = tk.Entry(width=45)
+email_username_entry.insert(0, "example@mail.com")
 email_username_entry.grid(row=2, column=1, columnspan=2)
-
-# Password Row
-password_label = tk.Label(text="Password:")
-password_label.grid(row=3, column=0)
 
 password_entry = tk.Entry(width=25)
 password_entry.grid(row=3, column=1)
 
+
+# Buttons
 gen_password_button = tk.Button(text="Generate Password")
 gen_password_button.grid(row=3, column=2)
 
-# Add Row
-add_button = tk.Button(text="Add", width=43)
+add_button = tk.Button(text="Add", width=43, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
