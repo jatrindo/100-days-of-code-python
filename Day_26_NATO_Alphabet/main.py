63e14a7c33f473f2bf5b92281e4b7242acfd7e89
jatrindo <jatrindo4096@gmail.com>
2021-12-03 20:39:08 -0600

    Add error handling to NATO Alphabet project
    The NATO Alphabet code can now gracefully handle non-alphabet letters
    that are given as input.
    
    Done as one of the Day 30 exception handling challenges.

--- Day_26_NATO_Alphabet/main.py
+++ Day_26_NATO_Alphabet/main.py
@@ -6,8 +6,12 @@ nato_df = pandas.read_csv('nato_phonetic_alphabet.csv')
 alphabet = {row.letter:row.code for (index, row) in nato_df.iterrows()}
 
 # 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ")
-
-coded_word = [alphabet[letter.upper()] for letter in word]
-print(coded_word)
-
+valid_word = False
+while not valid_word:
+    try:
+        word = input("Enter a word: ")
+        coded_word = [alphabet[letter.upper()] for letter in word]
+        valid_word = True
+        print(coded_word)
+    except KeyError:
+        print("Sorry, only letters of the English alphabet please.")
