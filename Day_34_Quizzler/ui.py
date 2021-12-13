1837137632d8b453bfdae71d490568b4f62d09c7
jatrindo <jatrindo4096@gmail.com>
2021-12-12 22:19:44 -0600

    Have Quizzler UI display Qs from the question bank

--- Day_34_Quizzler/ui.py
+++ Day_34_Quizzler/ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 WHITE = "#FFFFFF"
@@ -7,7 +8,8 @@ FONT = "Arial"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = tk.Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -20,11 +22,14 @@ class QuizInterface:
         self.canvas = tk.Canvas(width=300, height=250,
                                 highlightthickness=0, bg=WHITE)
         self.question_text = self.canvas.create_text(
-            150, 125, text="Placeholder Text",
-            font=(FONT, 20, 'italic'), fill=THEME_COLOR
+            150,
+            125,
+            width=280,
+            text="Placeholder Text",
+            font=(FONT, 20, 'italic'),
+            fill=THEME_COLOR
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
-
 
         # Buttons
         true_img = tk.PhotoImage(file="images/true.png")
@@ -35,4 +40,10 @@ class QuizInterface:
         self.false_button = tk.Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
